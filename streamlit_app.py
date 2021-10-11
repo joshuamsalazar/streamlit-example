d80cccaf379dac8e67a25c65e6ada03f6b279c30
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-10-11 20:59:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -296,57 +296,24 @@ def savedata(p, sig, fieldrangeT, name):
 #    return plot
   
 #stplot(fieldrangeT, signal2w, "H_ext", "V2w", "Harmonics", "line")
-p = figure(
-      title="title",
-      x_axis_label="Hext",
-      y_axis_label="V2w")
+#p = figure(
+#      title="title",
+#      x_axis_label="Hext",
+#      y_axis_label="V2w")
     
-p.line(fieldrangeT, signal2w, legend_label = "line", line_width=2)
+#p.line(fieldrangeT, signal2w, legend_label = "line", line_width=2)
+#st.bokeh_chart(p, use_container_width=True)
+#show(p)
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+     title='simple line example',
+     x_axis_label='x',
+     y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
 st.bokeh_chart(p, use_container_width=True)
-show(p)
 
 
-#from collections import namedtuple
-#import altair as alt
-#import math
-#import pandas as pd
-#import streamlit as st
-#import numpy as np
-
-#"""
-# Welcome to Streamlit!
-
-#Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-#If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-#forums](https://discuss.streamlit.io).#
-
-#In the meantime, below is an example of what you can do with just a few lines of code:
-#"""
-
-#chart_data = pd.DataFrame(
-#     np.random.randn(20, 3),
-#     columns=['a', 'b', 'c'])
-
-#st.line_chart(chart_data)
-
-
-#with st.echo(code_location='below'):
-#    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-#    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-#    Point = namedtuple('Point', 'x y')
-#    data = []
-
-#    points_per_turn = total_points / num_turns
-
-#    for curr_point_num in range(total_points):
-#        curr_turn, i = divmod(curr_point_num, points_per_turn)
-#        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#        radius = curr_point_num / total_points
-#        x = radius * math.cos(angle)
-#        y = radius * math.sin(angle)
-#        data.append(Point(x, y))
-
-#    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#        .mark_circle(color='#0068c9', opacity=0.5)
