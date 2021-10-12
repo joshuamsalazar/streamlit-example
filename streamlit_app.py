1c2b152d19cd423fc94372ea18a9c20757773d61
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-10-13 00:44:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,16 +21,16 @@ st.write('The following page describes the details to consider to efficiently si
 st.subheader('Theoretical description')
 st.write('The LLG equation epmloyeed in the model is in explicit form and takes the Slonczewsky spin-orbit-torque coefficients as input. It goes as follows:')
 st.latex(r''' \frac{\partial \vec{m}}{\partial t} =
-   \frac{\gamma}{1+\alpha^2} \vec{m} \times \vec{H}_{\text{eff}} - 
-   \frac{\gamma \alpha}{1+\alpha^2} \:\vec{m} \times \vec{m} \times \vec{H}_{\text{eff}}''')
+   \frac{\gamma}{1+\alpha^2} (\vec{m} \times \vec{H}_{\text{eff}}) - 
+   \frac{\gamma \alpha}{1+\alpha^2} \:(\vec{m} \times \vec{m} \times \vec{H}_{\text{eff}})''')
 st.write('Where $m$ represents the mgnetization unit vector, $\alpha$ the Gilbert damping constant, $\gamma$ the gyromagnetic ratio, and $\vec{H}_{eff}$ is the effective magnetic field. The effective magnetic field contains contributions of the applied external field, the effective anisotropy field, and the current induced fields via spin orbit torque effects. It reads as follows:')
 st.latex(r''' \vec{ H }_{\text{eff}} =
 \vec{ H }_{\text{ext}} + \vec{ H }_{\text{k}} + 
 \vec{ H }^{\text{SOT}}_{\text{FL}} + 
 \vec{ H }^{\text{SOT}}_{\text{DL}} \\ \:\\ \:\\
 \vec{ H }_{\text{k}} = \frac{2\vec{K}_1}{Js}  \\ \:\\
-\vec{ H }^{\text{SOT}}_{\text{FL}} = \eta_\text{FL} \frac{  j_e \hbar  }{ 2 e t \mu_0 M_s }\:\vec{m} \times \vec{m} \times \vec{p} \\ \:\\
-\vec{ H }^{\text{SOT}}_{\text{DL}} = \eta_\text{DL} \frac{  j_e \hbar  }{ 2 e t \mu_0 M_s }\:\vec{m} \times \vec{p}
+\vec{ H }^{\text{SOT}}_{\text{FL}} = \eta_\text{FL} \frac{  j_e \hbar  }{ 2 e t \mu_0 M_s }\:(\vec{m} \times \vec{m} \times \vec{p}) \\ \:\\
+\vec{ H }^{\text{SOT}}_{\text{DL}} = \eta_\text{DL} \frac{  j_e \hbar  }{ 2 e t \mu_0 M_s }\:(\vec{m} \times \vec{p})
 ''')
 
 
