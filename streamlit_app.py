8bc8df04e8c76ae2f37174a0cb848953260929c1
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-10-12 23:57:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,14 +29,12 @@ st.latex(r''' \vec{ H }_{\text{eff}} =
 \vec{ H }^{\text{SOT}}_{\text{FL}} + 
 \vec{ H }^{\text{SOT}}_{\text{DL}}  
 = \frac{1}{1}''')
-st.markdown(r'''
-$$
+st.latex(r'''
 \begin{equation}
 \begin{split}   a &=b+c\\
       &=e+f
 \end{split}
 \end{equation}
-$$
 ''')
 
 st.sidebar.markdown("## Parameters used in the simulation")
