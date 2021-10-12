2aa31f3d784098d5a99d9c97b00eaa38b4e96f76
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-10-12 23:52:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,11 @@ st.latex(r''' \frac{\partial \vec{m}}{\partial t} =
    \frac{\gamma}{1+\alpha^2} \vec{m} \times \vec{H}_{\text{eff}} - 
    \frac{\gamma \alpha}{1+\alpha^2} \:\vec{m} \times \vec{m} \times \vec{H}_{\text{eff}}''')
 st.write('Where $m$ represents the mgnetization unit vector, $\alpha$ the Gilbert damping constant, $\gamma$ the gyromagnetic ratio, and $\vec{H}_{eff}$ is the effective magnetic field. The effective magnetic field contains contributions of the applied external field, the effective anisotropy field, and the current induced fields via spin orbit torque effects. It reads as follows:')
-st.latex(r''' \vec{ H }_{\text{eff}} =\vec{ H }_{\text{ext}} + \vec{ H }_{\text{k}} + \vec{ H }^{\text{SOT}}_{\text{FL}} + \vec{ H }^{\text{SOT}}_{\text{DL}}  ''')
+st.latex(r''' \vec{ H }_{\text{eff}} &=
+\vec{ H }_{\text{ext}} + \vec{ H }_{\text{k}} + 
+\vec{ H }^{\text{SOT}}_{\text{FL}} + 
+\vec{ H }^{\text{SOT}}_{\text{DL}}  
+&= \frac{1}{1}''')
 
 st.sidebar.markdown("## Parameters used in the simulation")
 st.sidebar.markdown("Enter your own custom values to run the model")
